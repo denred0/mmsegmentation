5ac66720c701b396f20f419f28834a24630a6f50
vid <hah@videomatrix.ru>
2021-06-18 18:25:19 +0500

    custom dataset

--- denred0_src/inference.py
+++ denred0_src/inference.py
@@ -13,7 +13,8 @@ from classes import LASER_CLASSES, PALETTE
 from utils import get_all_files_in_folder, create_border
 
 
-def inference(exp_name, images_dir, checkpoint, images_ext, output_folder, create_visualization, device='cuda:0'):
+def inference(exp_name, images_dir, checkpoint, images_ext, data_root_cfg, output_folder, create_visualization,
+              device='cuda:0'):
     # create folders
     dirpath = output_folder.joinpath(exp_name)
     if dirpath.exists() and dirpath.is_dir():
@@ -25,7 +26,7 @@ def inference(exp_name, images_dir, checkpoint, images_ext, output_folder, creat
     Path(dirpath).mkdir(parents=True, exist_ok=True)
 
     # get config
-    _, datasets, cfg = create_config()
+    _, datasets, cfg = create_config(data_root=data_root_cfg, exp_name=exp_name)
 
     cfg.test_pipeline = [
         dict(type='LoadImageFromFile'),
@@ -59,7 +60,7 @@ def inference(exp_name, images_dir, checkpoint, images_ext, output_folder, creat
 
         if create_visualization:
             image_orig = cv2.imread(str(image_path), cv2.IMREAD_COLOR)
-            mask_orig = cv2.imread('denred0_data/data/masks_rgb/' + image_path.name, cv2.IMREAD_COLOR)
+            mask_orig = cv2.imread('denred0_data/data_train/masks_rgb/' + image_path.name, cv2.IMREAD_COLOR)
             mask_res = cv2.imread(
                 str(output_folder.joinpath(exp_name).joinpath('images_result').joinpath(image_path.name)),
                 cv2.IMREAD_COLOR)
@@ -75,14 +76,12 @@ def inference(exp_name, images_dir, checkpoint, images_ext, output_folder, creat
             cv2.imwrite(str(output_folder.joinpath(exp_name).joinpath('visualization').joinpath(image_path.name)), vis)
 
 
-
-
-
 if __name__ == '__main__':
-    checkpoint = 'denred0_work_dirs/deeplabv3plus_r101-d8_512x512_40k_voc12aug/iter_11000.pth'
-    images_dir = Path('denred0_data/data/imgs')
+    checkpoint = 'denred0_work_dirs/deeplabv3plus_r101-d8_512x512_40k_voc12aug_aug_dataset/iter_9000.pth'
+    images_dir = Path('denred0_data/data_train/images')
     images_ext = ['*.png']
-    exp_name = 'deeplabv3plus_r101_d8_512x512_40k_voc12aug'
+    data_root_cfg = 'denred0_data/data_train_augmentation/'
+    exp_name = 'deeplabv3plus_r101_d8_512x512_40k_voc12aug_aug_dataset'
     device = 'cuda:0'
     output_folder = Path('denred0_data/inference')
     create_visualization = True
@@ -91,6 +90,7 @@ if __name__ == '__main__':
               images_dir=images_dir,
               checkpoint=checkpoint,
               images_ext=images_ext,
+              data_root_cfg=data_root_cfg,
               output_folder=output_folder,
               create_visualization=create_visualization,
               device=device)
